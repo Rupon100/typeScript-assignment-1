ece96b1fd142a44b7bd5f1efffb639111f15d528
Rupon100 <ruponmia97@gmail.com>
2025-11-16 23:25:41 +0600

    remove console and fix some code

--- solution.ts
+++ solution.ts
@@ -1,5 +1,4 @@
-// 1
-const formatValue = (value: string | number | boolean) => {
+const formatValue = (value: string | number | boolean): string | number | boolean => {
     if(typeof value === 'string'){
         return value.toUpperCase();
     }else if(typeof value === 'number'){
@@ -8,25 +7,25 @@ const formatValue = (value: string | number | boolean) => {
         return !value;
     }
 }
-// console.log(formatValue('hello'));
-// console.log(formatValue(5));
-// console.log(formatValue(true));
+ 
 
 
-// 2
+
 type CheckParam = string | unknown[];
-const getLength = (value: CheckParam) => {
+const getLength = (value: CheckParam): number => {
     if(typeof value === 'string'){
         return value.length;
     }
     if(Array.isArray(value)){
         return value.length;
     }
+    return 0;
 }
-// console.log(getLength('typescript'));
-// console.log(getLength([10, 20, 30, 40]));
 
-// 3
+
+
+
+
 class Person {
     name: string
     age: number
@@ -39,14 +38,11 @@ class Person {
         return `Name: ${this.name}, Age: ${this.age}`
     }
 }
-// const person1 = new Person('John Doe', 30);
-// console.log(person1.getDetails());
-
-// const person2 = new Person('Alice', 25);
-// console.log(person2.getDetails());
+ 
 
 
-// 4
+
+
 type Values = {
     title: string;
     rating: number;
@@ -54,17 +50,11 @@ type Values = {
 const filterByRating = (values: Values[]): Values[] => {
     return values.filter(value => value.rating >= 4)
 }
-// const books = [
-//   { title: 'Book A', rating: 4.5 },
-//   { title: 'Book B', rating: 3.2 },
-//   { title: 'Book C', rating: 5.0 },
-// ];
-// console.log(filterByRating(books));
 
 
 
 
-// 5
+ 
 type Properties = {
     id: number;
     name: string;
@@ -74,21 +64,12 @@ type Properties = {
 const filterActiveUsers = (values: Properties[]): Properties[] => {
     return values.filter(value => value.isActive === true);
 }
-// const users = [
-//   { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
-//   { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
-//   { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
-// ];
-
-// console.log(filterActiveUsers(users));
 
 
 
 
 
 
-
-// 6
 interface Book {
     title: string;
     author: string;
@@ -98,33 +79,50 @@ interface Book {
 const printBookDetails = (book: Book) => {
     console.log(`Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${book.isAvailable === true ? 'Yes' : 'No'}`)
 }
-// const myBook: Book = {
-//   title: 'The Great Gatsby',
-//   author: 'F. Scott Fitzgerald',
-//   publishedYear: 1925,
-//   isAvailable: true,
-// };
-
-// printBookDetails(myBook);
 
 
+ 
 
 
-// 7
-type ArrayType = number[];
-const getUniqueValues = (array1: ArrayType, array2: ArrayType) => {
-    const newArr = array1.concat(array2);
-    return [...new Set(newArr)];
+const getUniqueValues = <T extends number | string>(array1: T[], array2: T[]): T[] => {
+    const result: T[] = [];
+
+    for(let i = 0; i < array1.length; i++){
+        let val = array1[i];
+        let flag = false;
+        for(let j = 0; j < result.length; j++){
+            if(result[j] === val){
+                flag = true;
+                break
+            }
+        }
+        if(!flag){
+            result.push(val);
+        }
+    }
+
+    for(let i = 0; i < array2.length; i++){
+        let val = array2[i];
+        let flag = false;
+        for(let j = 0; j < result.length; j++){
+            if(result[j] === val){
+                flag = true;
+                break;
+            }
+        }
+        if(!flag){
+            result.push(val)
+        }
+    }
+    return result;
 }
-// const array1 = [1, 2, 3, 4, 5];
-// const array2 = [3, 4, 5, 6, 7];
-// console.log(getUniqueValues(array1, array2));
 
 
 
 
 
-// 8
+
+
 interface Product {
     name: string;
     price: number;
@@ -133,17 +131,15 @@ interface Product {
 }
 const calculateTotalPrice = (products: Product[]): number => {
     if(products.length > 0){
-        const price = 0;
-        const totalPrice = products.reduce((acc, current) => acc + current.price*current.quantity ,0);
-        return totalPrice;
+        const reduceTotal = products.reduce((sum, current) => {
+            const price = current.quantity * current.price;
+            const discount = current.discount ? price * (current.discount / 100) : 0;
+            const diff = price - discount;
+            return diff + sum;
+        }, 0);
+        return reduceTotal;
     }
     return 0;
 }
-const products = [
-  { name: 'Pen', price: 10, quantity: 2 },
-  { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
-  { name: 'Bag', price: 50, quantity: 1, discount: 20 },
-];
-
-console.log(calculateTotalPrice(products));
+ 
 
